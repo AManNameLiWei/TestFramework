3b3ede087003f5d33191e2f4835aab77b10564cf
AManNameLiWei <51556966+AManNameLiWei@users.noreply.github.com>
2022-05-17 15:40:56 +0800

    Add files via upload

---
+++ TestFramework/TestFramework/LWLog.h
@@ -0,0 +1,16 @@
+//
+//  LWLog.h
+//  TestFramework
+//
+//  Created by 厉威 on 2022/5/17.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LWLog : NSObject
+- (void)lw_print:(NSString *)string;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestFramework/TestFramework/LWLog.m
@@ -0,0 +1,14 @@
+//
+//  LWLog.m
+//  TestFramework
+//
+//  Created by 厉威 on 2022/5/17.
+//
+
+#import "LWLog.h"
+
+@implementation LWLog
+- (void)lw_print:(NSString *)string {
+    NSLog(@"lw_print:  %@", string);
+}
+@end
